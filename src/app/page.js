14ec0a511e142236bd018fc7e439929f6c69f775
gsunil1996 <gajulasunilkumar123@gmail.com>
2023-11-13 17:41:30 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,45 @@
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
+import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import { useRefreshMutation } from "./redux/features/auth/authApiSlice";
+import { selectCurrentToken } from "./redux/features/auth/authSlice";
+
 export default function Home() {
   const router = useRouter();
+
+  const token = useSelector(selectCurrentToken);
+  const effectRan = useRef(false);
+
+  const [trueSuccess, setTrueSuccess] = useState(false);
+
+  const [refresh] = useRefreshMutation();
+
+  useEffect(() => {
+    if (effectRan.current === true || process.env.NODE_ENV !== "development") {
+      // React 18 Strict Mode
+
+      const verifyRefreshToken = async () => {
+        //  console.log('verifying refresh token')
+        try {
+          //const response =
+          await refresh();
+          //const { accessToken } = response.data
+          setTrueSuccess(true);
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
+      if (!token) verifyRefreshToken();
+    }
+
+    return () => (effectRan.current = true);
+
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <main className={styles.main}>
       <div>
